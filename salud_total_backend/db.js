0ed635d746ebdc2f12a90f5faa2acf1bfa1bfa8c
EmilioSarchetti <emiliosarchetti@gmail.com>
2025-04-14 18:36:39 -0300

    ds

--- salud_total_backend/db.js
+++ salud_total_backend/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '12345', // ajustá tu contraseña
-  database: 'salud_total'
+  database: 'saludtotal'
 });
 
 module.exports = pool;
